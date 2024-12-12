758706273074d50b8455a17addb966d39d04ff2e
Sam Andrei A. Bandiola <165779502+Smbndl@users.noreply.github.com>
2024-12-12 20:49:53 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function revealMessage() {
+  const message = document.getElementById('message');
+  message.classList.remove('hidden');
+  document.getElementById('revealButton').style.display = 'none';
+}
